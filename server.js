2c26e4b8eccd9d50b30b17bdce8755f1cba9bbc1
farahhadii <farahadi32@gmail.com>
2025-01-03 13:42:22 -0500

    update domain origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:false})) 
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["http://localhost:3000", "https://InventoryPro.vercel.app"],
+    origin: ["http://localhost:3000", "https://inventorypro.vercel.app"],
     credentials: true
 }));
 
